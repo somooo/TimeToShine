70f49fcb6f6064491533d981dd39b82b3dc7821b
somooo <somoooy@yahoo.com>
2023-10-08 10:18:47 +0300

    Initial Commit

---
+++ TimeToShine/TimeToShineApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimeToShineApp.swift
+//  TimeToShine
+//
+//  Created by bsamh on 23/03/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct TimeToShineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
